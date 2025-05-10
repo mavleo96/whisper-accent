4fc68ab74921ac6015b6c19384476fbade9e4595
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-05-09 23:13:02 -0400

    refactored codebook accent model code

---
+++ src/models/codebook_model.py
@@ -0,0 +1,150 @@
+import lightning as L
+import torch
+import torch.nn.functional as F
+from torchmetrics.text import WordErrorRate
+from transformers import WhisperForConditionalGeneration, WhisperProcessor
+
+from src.models.accent_codebook_model import WhisperWithAccentCodebook
+
+
+class CodebookWhisperModel(L.LightningModule):
+    """Lightning module for whisper models for baseline model pipelines"""
+
+    def __init__(self, model_name, optimizer_config):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.model = WhisperWithAccentCodebook.from_pretrained(model_name)
+        self.model = torch.compile(self.model)
+        self.processor = WhisperProcessor.from_pretrained(model_name)
+        self.optimizer_config = optimizer_config
+
+        # Reference Link: https://github.com/huggingface/transformers/pull/28687
+        if self.model.generation_config.is_multilingual:
+            self.model.generation_config.language = "<|en|>"
+            self.model.generation_config.task = "transcribe"
+
+        # wer defined on cpu for multi-gpu compatibility
+        self.val_wer = WordErrorRate(dist_sync_on_step=True, compute_on_cpu=True)
+        self.test_wer = WordErrorRate(dist_sync_on_step=True, compute_on_cpu=True)
+
+    def forward(
+        self, input_features, labels, attention_mask, decoder_attention_mask, **kwargs
+    ):
+        return self.model(
+            input_features,
+            attention_mask=attention_mask,
+            labels=labels,
+            decoder_attention_mask=decoder_attention_mask,
+        )
+
+    def generate(self, input_features, attention_mask, **kwargs):
+        predicted_ids = self.model.generate(
+            input_features, attention_mask=attention_mask
+        )
+        predicted_text = self.processor.batch_decode(
+            predicted_ids, skip_special_tokens=True
+        )
+        predicted_text = [
+            self.processor.tokenizer.normalize(text) for text in predicted_text
+        ]
+        return predicted_text
+
+    def training_step(self, batch, batch_idx):
+        outputs = self(**batch)
+        loss = F.cross_entropy(
+            outputs.logits.transpose(1, 2),
+            batch["labels"],
+            ignore_index=self.processor.tokenizer.pad_token_id,
+        )
+        self.log(
+            "train_loss",
+            loss,
+            sync_dist=True,
+            on_step=True,
+            on_epoch=True,
+            prog_bar=True,
+        )
+        return {"loss": loss}
+
+    def validation_step(self, batch, batch_idx):
+        outputs = self(**batch)
+        loss = F.cross_entropy(
+            outputs.logits.transpose(1, 2),
+            batch["labels"],
+            ignore_index=self.processor.tokenizer.pad_token_id,
+        )
+        self.log(
+            "val_loss", loss, sync_dist=True, on_step=True, on_epoch=True, prog_bar=True
+        )
+
+        # TODO: check if this is correct
+        # not normalizing the predicted text
+        predicted_text = self.generate(**batch)
+        target_text = self.processor.batch_decode(
+            batch["labels"], skip_special_tokens=True
+        )
+
+        self.val_wer.update(predicted_text, target_text)
+        return {"predictions": predicted_text, "targets": target_text, "val_loss": loss}
+
+    def on_validation_epoch_end(self):
+        wer_score = self.val_wer.compute()
+        self.log(
+            "val_wer",
+            wer_score,
+            sync_dist=True,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+        self.val_wer.reset()
+
+    def test_step(self, batch, batch_idx):
+        # TODO: check if this is correct
+        # not normalizing the predicted text
+        predicted_text = self.generate(**batch)
+        target_text = self.processor.batch_decode(
+            batch["labels"], skip_special_tokens=True
+        )
+
+        self.test_wer.update(predicted_text, target_text)
+        return {
+            "predictions": predicted_text,
+            "targets": target_text,
+        }
+
+    def on_test_epoch_end(self):
+        wer_score = self.test_wer.compute()
+        self.log(
+            "test_wer",
+            wer_score,
+            sync_dist=True,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+        self.test_wer.reset()
+
+    def configure_optimizers(self):
+        lr = self.optimizer_config.lr
+        weight_decay = self.optimizer_config.weight_decay
+        return torch.optim.AdamW(self.parameters(), lr=lr, weight_decay=weight_decay)
+
+
+if __name__ == "__main__":
+    from hydra import compose, initialize
+
+    with initialize(config_path="../../configs", version_base=None):
+        cfg = compose(config_name="baseline_eval.yaml")
+
+    model = BaseWhisperModel(**cfg.model).to("cuda")
+    batch = {
+        "input_features": torch.randn(16, 80, 3000).to("cuda"),
+        "attention_mask": torch.randint(0, 2, (16, 3000)).to("cuda"),
+        "labels": torch.randint(0, 51865, (16, 448)).to("cuda"),
+        "decoder_attention_mask": torch.randint(0, 2, (16, 448)).to("cuda"),
+    }
+
+    outputs = model.test_step(batch, 0)
+    print(outputs)
---
+++ src/scripts/codebook_eval.py
@@ -0,0 +1,54 @@
+import logging
+
+import hydra
+import lightning as L
+import torch
+from lightning.pytorch.loggers import TensorBoardLogger
+
+from src.callbacks import AccentWERCallback, PredictionSaverCallback
+from src.data import EdaccDataModule
+from src.models.codebook_model import CodebookWhisperModel
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+torch.set_float32_matmul_precision("high")
+
+
+@hydra.main(
+    config_path="../../configs", config_name="codebook_eval.yaml", version_base=None
+)
+def main(cfg):
+    logger.info(f"Initializing model: {cfg.model.model_name}")
+    model = CodebookWhisperModel(**cfg.model)
+
+    logger.info("Initializing data module")
+    data_module = EdaccDataModule(**cfg.data)
+
+    model_name = cfg.model.model_name.split("/")[-1]
+    tensorboard_logger = TensorBoardLogger(
+        save_dir=cfg.trainer.logger[0].save_dir,
+        name=f"baseline_eval_{model_name}",
+    )
+
+    logger.info("Initializing trainer")
+    trainer = L.Trainer(
+        logger=[tensorboard_logger],
+        devices=cfg.trainer.devices,
+        precision=cfg.trainer.precision,
+        accelerator=cfg.trainer.accelerator,
+        strategy=cfg.trainer.strategy,
+        enable_progress_bar=True,
+        callbacks=[PredictionSaverCallback(), AccentWERCallback()],
+    )
+
+    logger.info("Starting evaluation")
+    results = trainer.test(model, data_module)
+
+    logger.info("Evaluation results:")
+    for metric, value in results[0].items():
+        logger.info(f"{metric}: {value:.4f}")
+
+
+if __name__ == "__main__":
+    main()
