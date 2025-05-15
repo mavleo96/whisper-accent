bc1b2b69defa59cd67f61663daac84a75b4a3fe5
Vijayabharathi Murugan <vijaybharathi1998@hotmail.com>
2025-05-15 18:45:36 -0400

    remove old common voice module

--- src/constants.py
+++ src/constants.py
@@ -1,20 +1,18 @@
 SAMPLING_RATE = 16000
 
-NUM_ACCENTS = 14
+NUM_ACCENTS = 11
 
 ACCENT_TO_ID_MAP = {
-    'United States English': 0,
-    'India and South Asia (India, Pakistan, Sri Lanka)': 1,
-    'England English': 2,
-    'Canadian English': 3,
-    'Australian English': 4,
-    'Southern African (South Africa, Zimbabwe, Namibia)': 5,
-    'Irish English': 6,
-    'Scottish English': 7,
-    'New Zealand English': 8,
-    'Hong Kong Englis': 9,
-    'Malaysian English': 10,
-    'Welsh English' : 11,
-    'Filipino' :12,
-    'Singaporean English': 13
+    "Southern British English": 0,
+    "Mainstream US English": 1,
+    "Irish English": 2,
+    "Eastern European": 3,
+    "Italian": 4,
+    "Egyptian": 5,
+    "Vietnamese": 6,
+    "Chinese": 7,
+    "Indian English": 8,
+    "Indonesian English": 9,
+    "Indonesian": 9,
+    "Unknown": 10,
 }
--- src/data/common_voice.py
+++ src/data/common_voice.py
@@ -7,7 +7,24 @@ from datasets import Audio, Dataset, load_dataset
 from torch.utils.data import DataLoader
 from transformers import WhisperProcessor
 
-from src.constants import ACCENT_TO_ID_MAP, SAMPLING_RATE
+NUM_ACCENTS = 14
+
+ACCENT_TO_ID_MAP = {
+    "United States English": 0,
+    "India and South Asia (India, Pakistan, Sri Lanka)": 1,
+    "England English": 2,
+    "Canadian English": 3,
+    "Australian English": 4,
+    "Southern African (South Africa, Zimbabwe, Namibia)": 5,
+    "Irish English": 6,
+    "Scottish English": 7,
+    "New Zealand English": 8,
+    "Hong Kong Englis": 9,
+    "Malaysian English": 10,
+    "Welsh English": 11,
+    "Filipino": 12,
+    "Singaporean English": 13,
+}
 
 
 class CommonVoiceDataModule(L.LightningDataModule):
